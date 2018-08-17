e7851a7d4ed73a524b661708e9e3c02354ecb057
kstach <konrad.stach00@gmail.com>
2018-08-17 11:57:38 +0200

    Add sending e-mails using SendGrid
    Asynchronously sending e-mail when new user has been added.

--- src/main/java/com/example/internshipapp/InternshipAppApplication.java
+++ src/main/java/com/example/internshipapp/InternshipAppApplication.java
@@ -3,13 +3,30 @@ package com.example.internshipapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @EnableCaching
+@EnableAsync
 @SpringBootApplication
-public class InternshipAppApplication {
+public class InternshipAppApplication extends AsyncConfigurerSupport {
 
     public static void main(String[] args) {
         SpringApplication.run(InternshipAppApplication.class, args);
     }
 
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("send-mail");
+        executor.initialize();
+        return executor;
+    }
 }
+
---
+++ src/main/java/com/example/internshipapp/service/SendGridEmailService.java
@@ -0,0 +1,45 @@
+package com.example.internshipapp.service;
+
+import com.example.internshipapp.model.User;
+import com.sendgrid.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+@Async
+public class SendGridEmailService {
+
+
+    @Value("${sendgrid.api.key}")
+    private String sendGridApiKey;
+    private static final Logger logger = LoggerFactory.getLogger(SendGridEmailService.class);
+
+
+    public void sendMail(User savedUser) {
+
+        Email from = new Email("test@example.com");
+        String subject = "New user created";
+        Email to = new Email("konrad.stach00@gmail.com");
+        Content content = new Content("text/plain", String.format("New user %s %s saved in database", savedUser.getFirstName(), savedUser.getLastName()));
+        Mail mail = new Mail(from, subject, to, content);
+
+        SendGrid sg = new SendGrid(sendGridApiKey);
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            logger.error(ex.getMessage());
+        }
+    }
+}
